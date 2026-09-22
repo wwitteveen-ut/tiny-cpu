074d915462ed0cd4a5b71cc22ab148c6b422ee92
witteveenw <witteveenwessel@gmail.com>
2026-09-22 23:34:45 +0200

    Implemented the decoder into the tiny_cpu. Also updated the temp instructions in the instruction memory to align with the decoder

--- src/instruction_memory.v
+++ src/instruction_memory.v
@@ -7,10 +7,17 @@ module instruction_memory(
 
     //Temporary, later the instructions come form elsewhere
     initial begin
-        memory[0] = 32'hAAAAAAAA;
-        memory[1] = 32'hBBBBBBBB;
-        memory[2] = 32'hCCCCCCCC;
-        memory[3] = 32'hDDDDDDDD;
+        // ADD R3, R1, R2
+        memory[0] = {4'b0000, 4'd3, 4'd1, 4'd2, 16'b0};
+
+        // SUB R4, R3, R1
+        memory[1] = {4'b0001, 4'd4, 4'd3, 4'd1, 16'b0};
+
+        // AND R5, R1, R2
+        memory[2] = {4'b0010, 4'd5, 4'd1, 4'd2, 16'b0};
+
+        // OR R6, R3, R4
+        memory[3] = {4'b0011, 4'd6, 4'd3, 4'd4, 16'b0};
     end
 
     assign instruction = memory[pc[5:2]];
--- src/tiny_cpu.v
+++ src/tiny_cpu.v
@@ -6,6 +6,11 @@ module tiny_cpu(
     logic [31:0] pc;
     logic [31:0] next_pc;
     logic [31:0] instruction;
+        
+    logic [3:0] opcode;
+    logic [3:0] rd;
+    logic [3:0] rs1;
+    logic [3:0] rs2;
 
     program_counter pc_module (
         .clk(clk),
@@ -17,6 +22,14 @@ module tiny_cpu(
     instruction_memory instruction_memory_module  (
         .pc(pc),
         .instruction(instruction)
+    );
+
+    decoder decoder_module (
+        .instruction(instruction),
+        .opcode(opcode),
+        .rd(rd),
+        .rs1(rs1),
+        .rs2(rs2)
     );
 
     assign next_pc = pc + 4;
